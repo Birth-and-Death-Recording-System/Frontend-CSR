ecd3d679c12dc29ec7493f546bc7bc2fd8a7846c
PrinceOforiAnkamah <sabastainofori@gmail.com>
2024-04-06 11:08:25 +0100

    nationality

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { tokenInterceptor } from './token.interceptor';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), 
-    provideHttpClient(withFetch(), withInterceptors([tokenInterceptor])),]
+    provideHttpClient(withFetch(), withInterceptors([tokenInterceptor])), provideAnimationsAsync(),]
  
 };
