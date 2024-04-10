e78277687f30c699cc78952d2087f02794fd7ee6
PrinceOforiAnkamah <sabastainofori@gmail.com>
2024-04-10 08:46:14 +0100

    Profile component

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,60 +5,72 @@ import { Observable, catchError, tap, throwError } from 'rxjs';
 import { Router, RouterLink } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   constructor(private http: HttpClient, private router: Router) {}
 
   signIn(userData: SignInData) {
     return this.http
-      .post<userResponse>(`http://127.0.0.1:8000/login/`, userData).pipe(
-        tap(response => {
+      .post<userResponse>(`http://127.0.0.1:8000/login/`, userData)
+      .pipe(
+        tap((response) => {
           if (response && response.token) {
             this.storeToken(response.token);
-            this.setUsername(response.userData.username)
-            this.setUserId(response.userData.id)
+            this.setUsername(response.userData.username);
+            this.setUserId(response.userData.id);
           }
-        }),
+        })
       );
- 
   }
-      
-  
+
   // Handle the auth response by setting the cookie and user data
- 
+
   private storeToken(token: string) {
     localStorage.setItem('token', token);
   }
 
-  private setUsername(username: string){
-    sessionStorage.setItem('username', username)
+  private setUsername(username: string) {
+    sessionStorage.setItem('username', username);
   }
-  
-  private setUserId(id: string){
-    sessionStorage.setItem('id', id)
-    
+
+  private setUserId(id: string) {
+    sessionStorage.setItem('id', id);
   }
 
   getToken() {
-  const token= sessionStorage.getItem('token');
-  console.log(token, 'token');
-  return token;
-
+    const token = sessionStorage.getItem('token');
+    console.log(token, 'token');
+    return token;
   }
 
-  getUsername(){
-    const username =  sessionStorage.getItem("username")
-    return username
+  getUsername() {
+    const username = sessionStorage.getItem('username');
+    return username;
   }
 
-  getUserId(){
-    const userId = sessionStorage.getItem('id')
-    return userId
+  getUserId() {
+    const userId = sessionStorage.getItem('id');
+    return userId;
   }
 
-  
+  getProfileDetail() {
+    return this.http.get(`http://localhost:8000/profile/`).pipe(
+      catchError((error) => {
+        console.log(error);
+        return throwError(() => error); // Return the error as an Observable
+      })
+    );
+  }
+
+  updateProfileData(data: any): Observable<any> {
+    return this.http.put(`http://localhost:8000/profile/`, data).pipe(
+      catchError((error) => {
+        console.log(error);
+        return throwError(() => error); // Return the error as an Observable
+      })
+    );
+  }
 
   // Method to clear authentication state and log out the user
   logout(): void {
