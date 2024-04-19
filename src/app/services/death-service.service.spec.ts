7432935da0879f982ba11b469cbab90660e85f27
PrinceOforiAnkamah <sabastainofori@gmail.com>
2024-04-19 09:34:18 +0000

    Removed console logs

--- src/app/services/death-service.service.spec.ts
+++ src/app/services/death-service.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { DeathServiceService } from './death-service.service';
+import { DeathService } from './death-service.service';
 
 describe('DeathServiceService', () => {
-  let service: DeathServiceService;
+  let service: DeathService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(DeathServiceService);
+    service = TestBed.inject(DeathService);
   });
 
   it('should be created', () => {
