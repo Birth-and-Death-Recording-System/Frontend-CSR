5f1a6dc98f433f52f30d5e3997b31add8b0c283e
PrinceOforiAnkamah <sabastainofori@gmail.com>
2024-09-22 16:58:53 +0000

    fix authentication

--- src/app/token.interceptor.ts
+++ src/app/token.interceptor.ts
@@ -4,17 +4,21 @@ import { inject } from '@angular/core';
 import { catchError, tap, throwError } from 'rxjs';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const token = authService.getToken();
+  
+  // Determine if this request is for the login endpoint
+  const isLoginRequest = req.url.includes('/login/');
 
+  // Clone request to add the token, if present, and if not a login request
+  const authReq = (!isLoginRequest && token)
+    ? req.clone({
+        setHeaders: {
+          Authorization: `Token ${token}`,
+        },
+      })
+    : req; // If no token or login request, send the original request
 
-const authService = inject(AuthService);
-
-const authReq = req.clone({
-  setHeaders:{
-    Authorization: `Token ${authService.getToken()}`,
-
-  }
-}
-);
   return next(authReq)
-
+  
 };
